f32a4acd542d05f54003352a66c23f5669a92c73
anthonyrivera-tenantinc <anthony.rivera@tenantinc.com>
2026-04-01 23:06:13 +0000

    Add initial Django app structure with models, views, serializers, and database population script

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,34 @@
+from djongo import models
+from django.contrib.auth.models import AbstractUser
+
+
+class Team(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100, unique=True)
+    def __str__(self):
+        return self.name
+
+
+class User(AbstractUser):
+    id = models.ObjectIdField(primary_key=True, editable=False)
+    email = models.EmailField(unique=True)
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True, blank=True)
+
+
+class Activity(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    type = models.CharField(max_length=50)
+    duration = models.IntegerField()
+
+
+class Workout(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Leaderboard(models.Model):
+    id = models.ObjectIdField(primary_key=True, editable=False)
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    score = models.IntegerField()
